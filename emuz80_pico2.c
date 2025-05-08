170062fd67681a8c059b9bfafab45e9ad7cb071a
Norihiro Kumagai <tendai22plus@gmail.com>
2025-05-09 00:48:25 +0900

    MREQ/databus sm debugging, databus out is queer

--- emuz80_pico2.c
+++ emuz80_pico2.c
@@ -29,20 +29,20 @@ void clockgen_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint phase) {
     clockgen_program_init(pio, sm, offset, pin, phase);
 }
 
-void wait_mreq_forever(PIO pio, uint sm, uint offset, uint pin) {
-    wait_mreq_program_init(pio, sm, offset, pin);
+void wait_mreq_forever(PIO pio, uint sm, uint offset, uint rfsh_pin) {
+    wait_mreq_program_init(pio, sm, offset, rfsh_pin);
 }
 
-void wait_iorq_forever(PIO pio, uint sm, uint offset, uint pin) {
-    wait_iorq_program_init(pio, sm, offset, pin);
-}
+//void wait_iorq_forever(PIO pio, uint sm, uint offset, uint pin) {
+//    wait_iorq_program_init(pio, sm, offset, pin);
+//}
 
-void wait_access_forever(PIO pio, uint sm, uint offset) {
-    wait_access_program_init(pio, sm, offset);
-}
+//void wait_access_forever(PIO pio, uint sm, uint offset) {
+//    wait_access_program_init(pio, sm, offset);
+//}
 
-void databus_forever(PIO pio, uint sm, uint offset, uint debug_pin) {
-    databus_program_init(pio, sm, offset, debug_pin);
+void databus_forever(PIO pio, uint sm, uint offset, uint wait_pin) {
+    databus_program_init(pio, sm, offset, wait_pin);
 }
 
 // UART defines
@@ -83,8 +83,8 @@ int main()
     // In a default system, printf will also output via the default UART
     
     // Send out a string, with CR/LF conversions
-    uart_puts(UART_ID, " Hello, UART!\r\n");
-    printf("PICO_USE_GPIO_COPROCESSOR = %d, PICO_PIO_USE_GPIO_BASE = %d\n", PICO_USE_GPIO_COPROCESSOR, PICO_PIO_USE_GPIO_BASE);
+    //uart_puts(UART_ID, " Hello, UART!\r\n");
+    //printf("PICO_USE_GPIO_COPROCESSOR = %d, PICO_PIO_USE_GPIO_BASE = %d\n", PICO_USE_GPIO_COPROCESSOR, PICO_PIO_USE_GPIO_BASE);
     
     // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
 
@@ -98,7 +98,7 @@ int main()
     gpio_out_init(WAIT_Pin, true);
     gpio_out_init(RESET_Pin, false);
     gpio_out_init(BUSRQ_Pin, true);
-    //gpio_out_init(INT_Pin, false);      // INT Pin has an inverter, so negate signal is needed
+    gpio_out_init(INT_Pin, false);      // INT Pin has an inverter, so negate signal is needed
 
     gpio_out_init(TEST_Pin, true);
     TOGGLE();
@@ -119,7 +119,7 @@ int main()
     PIO pio_clock = pio0;
     PIO pio_wait = pio1;
     uint sm_clock = 0;
-        
+
     pio_set_gpio_base(pio_clock, 16);
     pio_set_gpio_base(pio_wait, 16);
     // pio_set_gpio_base should be invoked before pio_add_program
@@ -128,14 +128,14 @@ int main()
     clockgen_pin_forever(pio_clock, sm_clock, offset1, CLK_Pin, 1);
 
     offset1 = pio_add_program(pio_wait, &wait_mreq_program);
-    wait_mreq_forever(pio_wait, 0, offset1, WAIT_Pin);
-    offset1 = pio_add_program(pio_wait, &wait_iorq_program);
-    wait_iorq_forever(pio_wait, 1, offset1, WAIT_Pin);
+    wait_mreq_forever(pio_wait, 0, offset1, RFSH_Pin);
+    printf("offset1 = %d\n", offset1);
+    //offset1 = pio_add_program(pio_wait, &wait_iorq_program);
+    //wait_iorq_forever(pio_wait, 1, offset1, WAIT_Pin);
     offset1 = pio_add_program(pio_wait, &databus_program);
-    wait_access_forever(pio_wait, 2, offset1);
+    databus_forever(pio_wait, 2, offset1, WAIT_Pin);
+    printf("offset1 = %d\n", offset1);
 
-    offset1 = pio_add_program(pio, &databus_program);
-    databus_forever(pio, 1, offset1, DEBUG_Pin);
     // For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
     //int n = 10;
     //while (n-- > 0) TOGGLE();
@@ -149,35 +149,37 @@ int main()
     // start clock
     pio_sm_set_enabled(pio_clock, sm_clock, true);
     pio_sm_set_enabled(pio_wait, 0, true);
-    pio_sm_set_enabled(pio_wait, 1, true);  //IORQ not yet be ready 
+    //pio_sm_set_enabled(pio_wait, 1, true);  //IORQ not yet be ready 
     pio_sm_set_enabled(pio_wait, 2, true);
-    sleep_us(5);
+    sleep_us(10);
     TOGGLE();
     TOGGLE();
 
+    pio_sm_clear_fifos(pio_wait, 2);
     gpio_put(RESET_Pin, true);
 
     uint32_t addr, data, status;
+    uint32_t count = 0;
     while(true) {
-        // Wait for PIO1:IRQ0 High
-        while ((pio_wait->irq & 0x1) == 0)
-            ;
-        status = (gpio_get_all() >> 24) & 0xf;  // IORQ,MREQ,RD,M1
-        data = pio_sm_get_blocking(pio, 1);    // wait for access event occurs
-        if (status & 0x4) {     // Z80 Write mode: RD_Pin High
-            // Z80 Write
-            TOGGLE();
-            addr = gpio_get_all() & 0xffff;     // A0-A15 ... GPIO0-15
-            mem[addr] = data;
-            pio_sm_put(pio, 1, data);   // notify end of process
-            TOGGLE();
-        } else {            // Z80 Read mode: RD_Pin Low
-            // Z80 Read
-            TOGGLE();
-            addr = gpio_get_all() & 0xffff;     // A0-A15 ... GPIO0-15
-            data = mem[addr];
-            pio_sm_put(pio, 1, data);
-            TOGGLE();
+        if (pio_sm_is_rx_fifo_empty(pio_wait, 2) == 0) {
+            data = pio_sm_get_blocking(pio_wait, 2);    // wait for access event occurs
+            status = (gpio_get_all() >> 24) & 0xf;  // IORQ,MREQ,RD,M1
+            if (status & 0x4) {     // Z80 Write mode: RD_Pin High
+                // Z80 Write
+                TOGGLE();
+                addr = gpio_get_all() & 0xffff;     // A0-A15 ... GPIO0-15
+                mem[addr] = data;
+                pio_sm_put(pio_wait, 2, 21);   // notify end of process
+                TOGGLE();
+            } else {            // Z80 Read mode: RD_Pin Low
+                // Z80 Read
+                TOGGLE();
+                addr = gpio_get_all() & 0xffff;     // A0-A15 ... GPIO0-15
+                data = mem[addr];
+                pio_sm_put(pio_wait, 2, count);
+                count += 2;
+                TOGGLE();
+            }
         }
     }
 }
